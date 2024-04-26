9dc99e62147fb12f1c4106652df93b028da8ea2c
Sophia Ahn <sophiaseoyul2011@gmail.com>
2024-04-25 19:07:59 -0700

    Update SS-R5.py

--- SS-R5.py
+++ SS-R5.py
@@ -1,19 +1,19 @@
 from hub import light_matrix, motion_sensor, port
 import runloop, motor_pair, motor,math,time,runloop, color, color_sensor
 
+
 async def drive(distance, speed):
     CM = round(distance * 17.5)
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, CM, 0, velocity = speed, stop=motor.SMART_COAST)
 
 async def turnLeft(angle):
     while motion_sensor.tilt_angles()[0]<(angle*10): #while the angle sensor is less than desired angle
-        motor_pair.move(motor_pair.PAIR_1,-100) #both motors will run -100 degrees
+        motor_pair.move(motor_pair.PAIR_1,-100, acceleration=600, velocity=350) #both motors will run -100 degrees
     motor_pair.stop(motor_pair.PAIR_1) #stop the motors after that while loop
     motion_sensor.reset_yaw(0) #reset yaw value
-
 async def turnRight(angle):
     while motion_sensor.tilt_angles()[0]>(angle*-10): #getting yaw value from tuple
-        motor_pair.move(motor_pair.PAIR_1,100) #move to right
+        motor_pair.move(motor_pair.PAIR_1,100,acceleration=600, velocity=350) #move to right
     motor_pair.stop(motor_pair.PAIR_1) #stop the motors after that while loop
     motion_sensor.reset_yaw(0) #reset yaw value
 
@@ -28,16 +28,15 @@ async def moveMotor(degrees,speed, side):
     if (side == "right"):
         motor.run_for_degrees(port.A, degrees, speed, stop = motor.HOLD)
 
-
 async def main():
     # write your code here
     motion_sensor.reset_yaw(0)
     motor_pair.pair(motor_pair.PAIR_1,port.D,port.C)
     i = 0;
-    while i < 3:
-         await moveMotor(80,880,"left")
-         time.sleep_ms(1000)
-         await moveMotor(-80,880,"left")
-         time.sleep_ms(3000)
-         i += 1
+    while i < 64:
+        await moveMotor(80,890,"left")
+        time.sleep_ms(1000)
+        await moveMotor(-80,890,"left")
+        time.sleep_ms(2000)
+        i += 1
 runloop.run(main())
